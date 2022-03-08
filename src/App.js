37d5b15c4a843d82fe8618c6901a60fd39af330c
ahmed-razic <ahmed.razic@gmail.com>
2022-03-08 19:46:31 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Background from './components/Background';
 
+const BASE_URL = 'https://api.pexels.com/v1/search';
+
 function App() {
-    return (
-        <>
-            <Background />
-            <h1>Memory Game</h1>
-        </>
-    );
+  return (
+    <>
+      <Background />
+      <h1>Memory Game</h1>
+    </>
+  );
 }
 
 export default App;
